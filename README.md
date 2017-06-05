f165ae1c80379e876fcd5f4014d09fb90c71a680
michaelb <michaelpb@gmail.com>
2017-06-05 12:53:49 -0700

    Fully functioning rendering

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-jsc3d
-Node wrapper for JSC3D
+Node wrapper for JSC3D.
+
+Supports in-memory, software rendering of certain 3D Models with a simple
+interface, great for generating previews of STL or OBJ files.
+
+![Example render](examples/example.png)
+
+
--- index.js
+++ index.js
@@ -40,7 +40,7 @@ class XMLHttpRequest {
     setRequestHeader() {}
     overrideMimeType() {}
 
-    abort() {} // TODO implement to cancel fs.readFile
+    abort() {}
 
     send() {
         fs.readFile(this.path, (error, data) => {
@@ -49,9 +49,10 @@ class XMLHttpRequest {
             }
             this.status = 200;
             this.readyState = 4;
-            this.data = data;
-            this.responseText = data;
+            //this.data = data.toString();
+            this.responseText = data.toString();
             this.onreadystatechange(); // call callback
+            JSC3D.triggerLoaded(this.path); // read file, trigger ready
         });
     }
 }
@@ -61,20 +62,46 @@ const encodeURI = string => string; // don't actually encode anything
 // Include this in top level JSC3D to expose it as part of node-jsc3d's
 // interface
 const JSC3D = {
-    makeNodeCanvas: (width, height) => {
+    onLoadedCallbacks: [],
+    makeCairoCanvas: (width, height) => {
         const Canvas = require('canvas');
-        const canvas = new Canvas(1170, 585);
+        const canvas = new Canvas(width, height);
         canvas.addEventListener = () => {};
+        return canvas;
     },
-    makeSimpleCanvas: (width, height) => {
-        return {
-        };
+
+    render: (parameters, callback) => {
+        const {canvas} = parameters;
+        const viewer = new JSC3D.Viewer(canvas);
+        for (const name of Object.keys(parameters)) {
+            if (name === 'canvas') {
+                continue;
+            }
+            viewer.setParameter(name, parameters[name]);
+        }
+
+        viewer.init();
+        JSC3D.onLoaded(path => {
+            viewer.doUpdate();
+            callback();
+        });
+    },
+
+    // TODO: Replace with event emitter
+    onLoaded: callback => {
+        JSC3D.onLoadedCallbacks.push(callback);
+    },
+
+    triggerLoaded: path => {
+        for (const callback of JSC3D.onLoadedCallbacks) {
+            callback(path);
+        }
     },
 };
 
 function mockSetTimeout (func, time) {
-    // For now, run right away
-    setTimeout(func, 0);
+    // For now, actually STOP the setTimeout loop
+    // setTimeout(func, 0);
 }
 
 const sandbox = {
